94efb0f5e7de5138b87a1d3f8031df269615f7df
Marcelofury <marcelofury13579@gmail.com>
2026-06-11 14:17:58 +0300

    created push stack function on the array for demo

---
+++ arryayStack.js
@@ -0,0 +1,18 @@
+// STACK
+
+/// functions push, pop, peek, length
+
+var letters = [];
+
+var word = 'racecar'
+
+var rword = [];
+
+for(var i = 0; i <= word.length; i++ ){
+    letters.push(word[i])
+
+}
+
+for (var i = 0; i <= word.length){
+    
+}
