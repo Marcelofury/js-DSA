55c5b6bb2c52db3b21d153070365eb7fcc64678e
Marcelofury <marcelofury13579@gmail.com>
2026-06-11 15:55:17 +0300

    method to return all the values in the collection

--- sets.js
+++ sets.js
@@ -9,6 +9,10 @@ function mySet(){
    this.has = function(element){
     return (collection.indexOf(element) !== -1)
 }
+// this method will return all the values in the set
+this.values = function(){
+    return collection;
+}
 
 
 }
