eb965fe3d86e0b5eabf197bad0bb3bcf1b4fd5ee
Marcelofury <marcelofury13579@gmail.com>
2026-06-11 15:53:29 +0300

    collection values and function to check if the elements exist in the collection

---
+++ sets.js
@@ -0,0 +1,14 @@
+/** SETS */
+
+
+function mySet(){
+   // the var collection will hold the set
+
+   var collections = [];
+   //this method will check for the presence of an element and return true or false
+   this.has = function(element){
+    return (collection.indexOf(element) !== -1)
+}
+
+
+}
