9a9f3fd782ab64139e2f24398468098bfee3c745
Marcelofury <marcelofury13579@gmail.com>
2026-06-11 14:39:25 +0300

    function to push a value onto the stack

--- stack2.js
+++ stack2.js
@@ -3,4 +3,11 @@
 var Stack = function(){
     this.count = 0;
     this.storage = {};
+
+    // Adds a value onto the end of the stack
+
+    this.push = function(value){
+        this.storage[this.count] = value;
+        this.count++;
+    }
 }
