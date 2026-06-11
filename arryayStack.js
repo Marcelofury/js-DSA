5b832515cff20087a0aa8fcf6eb0336280c03b62
Marcelofury <marcelofury13579@gmail.com>
2026-06-11 14:23:14 +0300

    logic to prove stack and pop functions

--- arryayStack.js
+++ arryayStack.js
@@ -8,11 +8,21 @@ var word = 'racecar'
 
 var rword = [];
 
+
+// put letter of word into the stack
 for(var i = 0; i <= word.length; i++ ){
     letters.push(word[i])
 
 }
 
+// pop off the stack in reverse order
 for (var i = 0; i <= word.length){
-    rword.pop(word[i])
+    rword+= letters.pop()
+}
+
+if (word === rword){
+    console.log( word + 'is a palindrome')
+}
+else{
+console.log (word + 'is not  Palindrome')
 }
