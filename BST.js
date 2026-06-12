3196eff6ac7ba5f515785ae4b7f120856746c484
Marcelofury <marcelofury13579@gmail.com>
2026-06-12 16:29:28 +0300

    BST, add, find Min. findMax, finding data

--- BST.js
+++ BST.js
@@ -7,3 +7,67 @@ class Node{
         this.right = right;
     }
 }
+
+class BST{
+    constructor(){
+        this.root = null;
+    }
+    add(data){
+        const node = this.root;
+        if (node === null){
+            this.root = new Node(data);
+            return;
+        }else{
+            const searchTree = function (node){
+                if(data < node.data){
+                    if(node.left ===null){
+                        node.left = new Node(data);
+                        return
+                    }else if(node.left!= null){
+                        return searchTree(node.left);
+
+                    }
+                } else if (data > node.data){
+                    if(node.right === null){
+                        node.right = new Node(data);
+                        return
+                    }else if (node.right !== null){
+                        return searchTree(node.right)
+                    }
+                }else{
+                    return null;
+                }
+            }
+            return searchTree(node);
+        }
+    }
+
+findMin(){
+    let current = this.root;
+    while(current.left !== null){
+        current = current.left;
+    }
+    return current.data;
+}
+
+findMax(){
+    let current = this.root;
+    while(current.data !== null){
+        current = current.data;
+
+    }
+    return current.data;
+}
+
+find(data){
+    let current = this.root;
+    while (current.data !== data){
+        if(data < current.data){
+            current = current.lefy;
+        }
+    }
+}
+
+
+
+}
