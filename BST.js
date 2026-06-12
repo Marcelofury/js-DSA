4d15512ad9dc6fb24b3834ca0e680a4f81e1c690
Marcelofury <marcelofury13579@gmail.com>
2026-06-12 16:05:36 +0300

    Node class with a constructor with BST properties

---
+++ BST.js
@@ -0,0 +1,9 @@
+/**     BINARY SEARCH TREE */
+
+class Node{
+    constructor (data, left = null, right = null){
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
